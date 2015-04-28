ea06c26f62d4ec8eae559c64431e1b6b41eae9ec
Christian Andres Arteaga <caarteagag@gmail.com>
2015-04-27 23:55:10 -0500

    Comit Inicial
    Carga del proyecto

---
+++ LulzoInyector/LulzoInyector/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Libreria:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Proceso:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(63, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(63, 34)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(199, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(268, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Examinar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(268, 32)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Inyectar!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(11, 60)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(332, 147)
+        Me.ListBox1.TabIndex = 6
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "Libreria DLL|*.dll"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(357, 216)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lulzo DLL Inyector"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ LulzoInyector/LulzoInyector/Form1.vb
@@ -0,0 +1,184 @@
+﻿Public Class Form1
+    Private TargetProcessHandle As Integer
+    Private pfnStartAddr As Integer
+    Private pszLibFileRemote As String
+    Private TargetBufferSize As Integer
+
+    Public Const PROCESS_VM_READ = &H10
+    Public Const TH32CS_SNAPPROCESS = &H2
+    Public Const MEM_COMMIT = 4096
+    Public Const PAGE_READWRITE = 4
+    Public Const PROCESS_CREATE_THREAD = (&H2)
+    Public Const PROCESS_VM_OPERATION = (&H8)
+    Public Const PROCESS_VM_WRITE = (&H20)
+    Dim DLLFileName As String
+    Public Declare Function ReadProcessMemory Lib "kernel32" ( _
+    ByVal hProcess As Integer, _
+    ByVal lpBaseAddress As Integer, _
+    ByVal lpBuffer As String, _
+    ByVal nSize As Integer, _
+    ByRef lpNumberOfBytesWritten As Integer) As Integer
+
+    Public Declare Function LoadLibrary Lib "kernel32" Alias "LoadLibraryA" ( _
+    ByVal lpLibFileName As String) As Integer
+
+    Public Declare Function VirtualAllocEx Lib "kernel32" ( _
+    ByVal hProcess As Integer, _
+    ByVal lpAddress As Integer, _
+    ByVal dwSize As Integer, _
+    ByVal flAllocationType As Integer, _
+    ByVal flProtect As Integer) As Integer
+
+    Public Declare Function WriteProcessMemory Lib "kernel32" ( _
+    ByVal hProcess As Integer, _
+    ByVal lpBaseAddress As Integer, _
+    ByVal lpBuffer As String, _
+    ByVal nSize As Integer, _
+    ByRef lpNumberOfBytesWritten As Integer) As Integer
+
+    Public Declare Function GetProcAddress Lib "kernel32" ( _
+    ByVal hModule As Integer, ByVal lpProcName As String) As Integer
+
+    Private Declare Function GetModuleHandle Lib "Kernel32" Alias "GetModuleHandleA" ( _
+    ByVal lpModuleName As String) As Integer
+
+    Public Declare Function CreateRemoteThread Lib "kernel32" ( _
+    ByVal hProcess As Integer, _
+    ByVal lpThreadAttributes As Integer, _
+    ByVal dwStackSize As Integer, _
+    ByVal lpStartAddress As Integer, _
+    ByVal lpParameter As Integer, _
+    ByVal dwCreationFlags As Integer, _
+    ByRef lpThreadId As Integer) As Integer
+
+    Public Declare Function OpenProcess Lib "kernel32" ( _
+    ByVal dwDesiredAccess As Integer, _
+    ByVal bInheritHandle As Integer, _
+    ByVal dwProcessId As Integer) As Integer
+
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" ( _
+    ByVal lpClassName As String, _
+    ByVal lpWindowName As String) As Integer
+
+    Private Declare Function CloseHandle Lib "kernel32" Alias "CloseHandleA" ( _
+    ByVal hObject As Integer) As Integer
+
+    Dim ExeName As String = IO.Path.GetFileNameWithoutExtension(Application.ExecutablePath)
+
+    Dim dllproc As String = "0"
+    Dim aa As String = "0"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Lulzo Dll Inyector iniciado.")
+        cargarconfig()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        If OpenFileDialog1.FileName.Length > 0 Then
+            TextBox1.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text.Length > 0 Then
+            Button2.Enabled = True
+        Else
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If IO.File.Exists(dllproc) Then
+            Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox2.Text)
+            If TargetProcess.Length = 0 Then
+                If aa = "0" Then
+                    ListBox1.Items.Add("Esperando a: " + TextBox2.Text + ".exe")
+                    Button2.Text = "Cancelar"
+                    aa = "1"
+                End If
+            Else
+                Timer1.Stop()
+                ListBox1.Items.Add("Injectando..")
+                Call Inject()
+                End
+                End
+            End If
+        Else
+            ListBox1.Items.Add("Error: Dll no encontrado")
+        End If
+    End Sub
+
+    Private Sub Inject()
+        On Error GoTo 1
+        Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox2.Text)
+        TargetProcessHandle = OpenProcess(PROCESS_CREATE_THREAD Or PROCESS_VM_OPERATION Or PROCESS_VM_WRITE, False, TargetProcess(0).Id)
+        pszLibFileRemote = dllproc
+        pfnStartAddr = GetProcAddress(GetModuleHandle("Kernel32"), "LoadLibraryA")
+        TargetBufferSize = 1 + Len(pszLibFileRemote)
+        Dim Rtn As Integer
+        Dim LoadLibParamAdr As Integer
+        LoadLibParamAdr = VirtualAllocEx(TargetProcessHandle, 0, TargetBufferSize, MEM_COMMIT, PAGE_READWRITE)
+        Rtn = WriteProcessMemory(TargetProcessHandle, LoadLibParamAdr, pszLibFileRemote, TargetBufferSize, 0)
+        CreateRemoteThread(TargetProcessHandle, 0, 0, pfnStartAddr, LoadLibParamAdr, 0, 0)
+        CloseHandle(TargetProcessHandle)
+1:      End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Button2.Text = "Cancelar" Then
+            Button2.Text = "Inyectar!"
+            Timer1.Stop()
+            ListBox1.Items.Add("Espera cancelada.")
+            aa = 0
+            Exit Sub
+        End If
+
+        dllproc = OpenFileDialog1.FileName
+        If TextBox2.Text.Length = 0 Then
+            ListBox1.Items.Add("Por favor seleccione un proceso..")
+        Else
+            If dllproc = "0" Then
+                ListBox1.Items.Add("No se ha encontrado el DLL..")
+            Else
+                ListBox1.Items.Add("Dll Encontrado..")
+                guardarconfig()
+                dllproc = TextBox1.Text
+                Timer1.Start()
+            End If
+        End If
+    End Sub
+
+    Sub guardarconfig()
+        Try
+            Dim FILE_NAME As String = "InyectConfig.ini"
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+            objWriter.WriteLine(TextBox1.Text)
+            objWriter.WriteLine(TextBox2.Text)
+            objWriter.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Sub cargarconfig()
+        Try
+            Dim FILE_NAME As String = "InyectConfig.ini"
+            Dim TextLine As String
+            Dim list As New ArrayList
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Dim objReader As New System.IO.StreamReader(FILE_NAME)
+                Do While objReader.Peek() <> -1
+                    TextLine = objReader.ReadLine()
+                    list.Add(TextLine)
+                Loop
+                TextBox1.Text = list(0)
+                TextBox2.Text = list(1)
+                ListBox1.Items.Add("Configuracion cargada.")
+                objReader.Close()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
